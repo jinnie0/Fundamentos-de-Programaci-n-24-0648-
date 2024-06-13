e81c57ba30f17ed4d662ecf9bd587554b0844849
jinnie0 <jinniesoto@gmail.com>
2024-06-12 20:30:28 -0400

    Add files via upload
    2. Operaciones matemáticas: Utiliza listas o tuplas para realizar operaciones matemáticas en conjuntos de datos. Por ejemplo, puedes sumar los elementos de una lista, obtener el producto de una tupla o calcular la media de una lista de números.

---
+++ #Operaciones matemáticas.py
@@ -0,0 +1,15 @@
+#Operaciones matemáticas 
+
+lista= [1, 2, 3, 4, 5]
+
+suma_lista = sum(lista)
+
+print(suma_lista)
+
+tupla = (1, 2, 3, 4, 5)
+for elementos in tupla:
+    print(elementos*2)
+
+notas = [90, 40, 50, 80]
+promedio = sum(notas)/len(notas)
+print(promedio)
