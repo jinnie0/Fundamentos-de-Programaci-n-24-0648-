559fb4fab750e12017d9cbfc6db879cc49739e09
jinnie0 <jinniesoto@gmail.com>
2024-06-12 20:30:58 -0400

    Add files via upload
    3.Acceder y modificar elementos: Utiliza índices para acceder a elementos específicos en listas, tuplas o diccionarios. También puedes modificar los valores de los elementos si es necesario.

---
+++ #Acceder y modificar elementos.py
@@ -0,0 +1,11 @@
+#Acceder y modificar elementos
+
+tupla= ("María", "Juana", "Xintao", "Vanessa", "Odile", "Camilo", "Orietta", "Jinnie")
+
+nuevatupla=tupla+("Pedro", )
+print(nuevatupla)
+
+diccionario= {1: "Juan", 2: "María", 3: "Xintao", 4: "Vanessa", 5: "Odile", 6: "Camilo", 7: "Orietta", 8: "Jinnie"}
+diccionario[9]= "Edwi"
+print (diccionario)
+
