2dfffd3441687c679624f887f0e0262a2f85557c
jinnie0 <jinniesoto@gmail.com>
2024-07-01 15:37:03 -0400

    Delete CICLOS directory

--- CICLOS/#Ejercicio 01.py
+++
@@ -1,9 +0,0 @@
-#Leer un número entero y mostrar todos los enteros comprendidos entre 1 y el número leído.
-
-x= int(input("Escribe un número: "))
-
-while x>1:
-    x= x-1
-    print (x)
-
-print("Esos son todos los números comprendidos entre 1 y el número que escribiste.")  
